7e6185d4431e74608a897ba58302bdfdae2e940c
KamasamaK <KamasamaK@users.noreply.github.com>
2018-10-27 21:55:27 -0400

    v1.5

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Giant Bomb reviewer images
 
-Reviewer images were removed upon a redesign of Giant Bomb. This script places a contextual cartoon image of the Giant Bomb reviewer into the space where the reviewer's image would previously be -- next to the star rating -- for all of their respective reviews on Giant Bomb. It additionally uses some non-review images for people who were not typical reviewers or had joined the site later.
+Reviewer images were removed upon a redesign of Giant Bomb. This script places a contextual cartoon image of the Giant Bomb reviewer into the position where the reviewer's image would previously be -- next to the star rating -- for all of their respective reviews on Giant Bomb. 
+
+People who were not typical reviewers or had joined the website later will instead have placeholder or no images. New art is welcome and will be incorporated in any of these cases if someone is inclined to create it. This script is subject to fail on a future redesign of the website.
 
 Images for the original members were created by [Ben Coello](https://www.giantbomb.com/ben-coello/3040-94726/). Images for Alex and Patrick were created by Giant Bomb user [papercut](https://www.giantbomb.com/profile/papercut/).
 
--- gb-review-images.user.js
+++ gb-review-images.user.js
@@ -1,98 +1,102 @@
 // ==UserScript==
-// @name        Giant Bomb review images
+// @name        Giant Bomb reviewer images
 // @namespace   https://github.com/KamasamaK/gb-review-images
-// @description Inserts old review images into newer site design
+// @description Inserts reviewer images into reviews on newer website designs
 // @match       https://*.giantbomb.com/reviews/*
-// @version     1.40
+// @homepageURL https://github.com/KamasamaK/gb-review-images
+// @supportURL  https://github.com/KamasamaK/gb-review-images/issues
+// @version     1.5
 // @author      KamasamaK
 // @grant       none
 // @license     MIT
 // ==/UserScript==
 
 /*
- This script is subject to fail on a redesign of the site
+ This script is subject to fail on a future redesign of the website
  Thanks to Ben Coello for creating the images for the original members
  Thanks to Giant Bomb user papercut for providing the Alex and Patrick images used here
 */
 (function () {
     "use strict";
-
-    var parentNode = document.getElementsByClassName("news-hdr")[0];
-    var reviewerName = document.getElementsByClassName("news-byline")[0].getElementsByTagName("a")[0].textContent;
-    var scoreNode = document.getElementsByClassName("score score-big score-special")[0];
-    var numberScore = scoreNode.getElementsByTagName("span")[0].textContent;
-    var systemList = document.getElementsByClassName("system-list")[0];
-
-    var imgNode = document.createElement("img");
-    imgNode.setAttribute("height", "150");
-    imgNode.setAttribute("align", "left");
-
-    var reviewImages = new Object();
-
-    reviewImages["Jeff Gerstmann"] = new Object();
-    reviewImages["Jeff Gerstmann"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2492756-jeff-1.png";
-    reviewImages["Jeff Gerstmann"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2492757-jeff-2.png";
-    reviewImages["Jeff Gerstmann"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2492758-jeff-3.png";
-    reviewImages["Jeff Gerstmann"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2492759-jeff-4.png";
-    reviewImages["Jeff Gerstmann"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2492760-jeff-5.png";
-
-    reviewImages["Brad Shoemaker"] = new Object();
-    reviewImages["Brad Shoemaker"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2492750-brad-1.png";
-    reviewImages["Brad Shoemaker"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2492751-brad-2.png";
-    reviewImages["Brad Shoemaker"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2492753-brad-3.png";
-    reviewImages["Brad Shoemaker"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2492754-brad-4.png";
-    reviewImages["Brad Shoemaker"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2492755-brad-5.png";
-
-    reviewImages["Ryan Davis"] = new Object();
-    reviewImages["Ryan Davis"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2492761-ryan-1.png";
-    reviewImages["Ryan Davis"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2492762-ryan-2.png";
-    reviewImages["Ryan Davis"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2492763-ryan-3.png";
-    reviewImages["Ryan Davis"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2492764-ryan-4.png";
-    reviewImages["Ryan Davis"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2492765-ryan-5.png";
-
-    reviewImages["Vinny Caravella"] = new Object();
-    reviewImages["Vinny Caravella"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2492766-vinny-1.png";
-    reviewImages["Vinny Caravella"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2492767-vinny-2.png";
-    reviewImages["Vinny Caravella"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2492768-vinny-3.png";
-    reviewImages["Vinny Caravella"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2492769-vinny-4.png";
-    reviewImages["Vinny Caravella"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2492770-vinny-5.png";
-
-    reviewImages["Alex Navarro"] = new Object();
-    reviewImages["Alex Navarro"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2337134-alex_1.png";
-    reviewImages["Alex Navarro"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2337135-alex_2.png";
-    reviewImages["Alex Navarro"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2337136-alex_3.png";
-    reviewImages["Alex Navarro"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2337137-alex_4.png";
-    reviewImages["Alex Navarro"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2337138-alex_5.png";
-
-    reviewImages["Patrick Klepek"] = new Object();
-    reviewImages["Patrick Klepek"]["1"] = "https://static.giantbomb.com/uploads/original/0/9253/2336676-patrick_1.png";
-    reviewImages["Patrick Klepek"]["2"] = "https://static.giantbomb.com/uploads/original/0/9253/2336677-patrick_2.png";
-    reviewImages["Patrick Klepek"]["3"] = "https://static.giantbomb.com/uploads/original/0/9253/2336678-patrick_3.png";
-    reviewImages["Patrick Klepek"]["4"] = "https://static.giantbomb.com/uploads/original/0/9253/2336679-patrick_4.png";
-    reviewImages["Patrick Klepek"]["5"] = "https://static.giantbomb.com/uploads/original/0/9253/2336680-patrick_5.png";
-
-    reviewImages["Dave Snider"] = new Object();
-    reviewImages["Drew Scanlon"] = new Object();
-    reviewImages["Matt Kessler"] = new Object();
-    reviewImages["Andy McCurdy"] = new Object();
-    for (var i = 1; i <= 5; i++) {
-        var numStr = i.toString();
-        reviewImages["Dave Snider"][numStr] = "https://static.giantbomb.com/uploads/original/0/9253/2492775-snide.png";
-        reviewImages["Drew Scanlon"][numStr] = "https://static.giantbomb.com/uploads/original/0/9253/2492773-drewbert.png";
-        reviewImages["Matt Kessler"][numStr] = "https://static.giantbomb.com/uploads/original/0/9253/2492774-mattbodega.png";
-        reviewImages["Andy McCurdy"][numStr] = "https://static.giantbomb.com/uploads/original/0/9253/2492772-andy.png";
+    
+    var parentElems = document.getElementsByClassName("news-hdr");
+    if (parentElems.length === 0) {
+        return false;
     }
+    var parentElem = parentElems[0];
+    
+    var reviewerName = document.getElementsByClassName("news-byline")[0].getElementsByTagName("a")[0].textContent;
+    var scoreElem = document.getElementsByClassName("score score-big score-special")[0];
+    var numberScore = scoreElem.getElementsByTagName("span")[0].textContent;
+    var systemListElem = document.getElementsByClassName("system-list")[0];
 
-    if (reviewImages.hasOwnProperty(reviewerName) && reviewImages[reviewerName][numberScore]) {
-        imgNode.setAttribute("src", reviewImages[reviewerName][numberScore]);
+    var imgElem = document.createElement("img");
+    imgElem.setAttribute("height", "150");
+    imgElem.setAttribute("align", "left");
+
+    var reviewerImages = new Object();
+    reviewerImages["Jeff Gerstmann"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492756-jeff-1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492757-jeff-2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492758-jeff-3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492759-jeff-4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492760-jeff-5.png"
+    };
+    reviewerImages["Brad Shoemaker"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492750-brad-1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492751-brad-2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492753-brad-3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492754-brad-4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492755-brad-5.png"
+    };
+    reviewerImages["Ryan Davis"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492761-ryan-1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492762-ryan-2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492763-ryan-3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492764-ryan-4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492765-ryan-5.png"
+    };
+    reviewerImages["Vinny Caravella"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492766-vinny-1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492767-vinny-2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492768-vinny-3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492769-vinny-4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492770-vinny-5.png"
+    };
+    reviewerImages["Alex Navarro"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2337134-alex_1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2337135-alex_2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2337136-alex_3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2337137-alex_4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2337138-alex_5.png"
+    };
+    reviewerImages["Patrick Klepek"] = {
+        "1": "https://static.giantbomb.com/uploads/original/0/9253/2336676-patrick_1.png",
+        "2": "https://static.giantbomb.com/uploads/original/0/9253/2336677-patrick_2.png",
+        "3": "https://static.giantbomb.com/uploads/original/0/9253/2336678-patrick_3.png",
+        "4": "https://static.giantbomb.com/uploads/original/0/9253/2336679-patrick_4.png",
+        "5": "https://static.giantbomb.com/uploads/original/0/9253/2336680-patrick_5.png"
+    };
+
+    var additionalReviewerImages = {
+        "Dave Snider": "https://static.giantbomb.com/uploads/original/0/9253/2492775-snide.png",
+        "Drew Scanlon": "https://static.giantbomb.com/uploads/original/0/9253/2492773-drewbert.png",
+        "Matt Kessler": "https://static.giantbomb.com/uploads/original/0/9253/2492774-mattbodega.png",
+        "Andy McCurdy": "https://static.giantbomb.com/uploads/original/0/9253/2492772-andy.png"
+    };
+
+    if (reviewerImages.hasOwnProperty(reviewerName) && reviewerImages[reviewerName][numberScore]) {
+        imgElem.setAttribute("src", reviewerImages[reviewerName][numberScore]);
+    } else if (additionalReviewerImages.hasOwnProperty(reviewerName)) {
+        imgElem.setAttribute("src", additionalReviewerImages[reviewerName]);
     } else {
         return false;
     }
 
-    parentNode.insertBefore(imgNode, scoreNode);
+    parentElem.insertBefore(imgElem, scoreElem);
     // Use CSS instead?
-    parentNode.insertBefore(document.createElement("br"), scoreNode);
-    parentNode.insertBefore(document.createElement("br"), systemList);
-    parentNode.insertBefore(document.createElement("br"), systemList);
-    parentNode.insertBefore(document.createElement("br"), systemList);
+    parentElem.insertBefore(document.createElement("br"), scoreElem);
+    parentElem.insertBefore(document.createElement("br"), systemListElem);
+    parentElem.insertBefore(document.createElement("br"), systemListElem);
+    parentElem.insertBefore(document.createElement("br"), systemListElem);
 })();
