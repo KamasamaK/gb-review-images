26948aa6238a27892f9dba90e4bc18186e85f715
KamasamaK <KamasamaK@users.noreply.github.com>
2021-12-06 06:36:10 -0500

    Fixed image links

--- gb-review-images.user.js
+++ gb-review-images.user.js
@@ -5,7 +5,7 @@
 // @match       https://*.giantbomb.com/reviews/*
 // @homepageURL https://github.com/KamasamaK/gb-review-images
 // @supportURL  https://github.com/KamasamaK/gb-review-images/issues
-// @version     1.5.1
+// @version     1.5.2
 // @author      KamasamaK
 // @grant       none
 // @license     MIT
@@ -36,53 +36,53 @@
 
     var reviewerImages = {};
     reviewerImages["Jeff Gerstmann"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492756-jeff-1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492757-jeff-2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492758-jeff-3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492759-jeff-4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492760-jeff-5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2492756-jeff-1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2492757-jeff-2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2492758-jeff-3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2492759-jeff-4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2492760-jeff-5.png"
     };
     reviewerImages["Brad Shoemaker"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492750-brad-1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492751-brad-2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492753-brad-3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492754-brad-4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492755-brad-5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2492750-brad-1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2492751-brad-2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2492753-brad-3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2492754-brad-4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2492755-brad-5.png"
     };
     reviewerImages["Ryan Davis"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492761-ryan-1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492762-ryan-2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492763-ryan-3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492764-ryan-4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492765-ryan-5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2492761-ryan-1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2492762-ryan-2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2492763-ryan-3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2492764-ryan-4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2492765-ryan-5.png"
     };
     reviewerImages["Vinny Caravella"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2492766-vinny-1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2492767-vinny-2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2492768-vinny-3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2492769-vinny-4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2492770-vinny-5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2492766-vinny-1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2492767-vinny-2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2492768-vinny-3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2492769-vinny-4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2492770-vinny-5.png"
     };
     reviewerImages["Alex Navarro"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2337134-alex_1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2337135-alex_2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2337136-alex_3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2337137-alex_4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2337138-alex_5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2337134-alex_1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2337135-alex_2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2337136-alex_3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2337137-alex_4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2337138-alex_5.png"
     };
     reviewerImages["Patrick Klepek"] = {
-        "1": "https://static.giantbomb.com/uploads/original/0/9253/2336676-patrick_1.png",
-        "2": "https://static.giantbomb.com/uploads/original/0/9253/2336677-patrick_2.png",
-        "3": "https://static.giantbomb.com/uploads/original/0/9253/2336678-patrick_3.png",
-        "4": "https://static.giantbomb.com/uploads/original/0/9253/2336679-patrick_4.png",
-        "5": "https://static.giantbomb.com/uploads/original/0/9253/2336680-patrick_5.png"
+        "1": "https://www.giantbomb.com/a/uploads/original/0/9253/2336676-patrick_1.png",
+        "2": "https://www.giantbomb.com/a/uploads/original/0/9253/2336677-patrick_2.png",
+        "3": "https://www.giantbomb.com/a/uploads/original/0/9253/2336678-patrick_3.png",
+        "4": "https://www.giantbomb.com/a/uploads/original/0/9253/2336679-patrick_4.png",
+        "5": "https://www.giantbomb.com/a/uploads/original/0/9253/2336680-patrick_5.png"
     };
 
     var additionalReviewerImages = {
-        "Dave Snider": "https://static.giantbomb.com/uploads/original/0/9253/2492775-snide.png",
-        "Drew Scanlon": "https://static.giantbomb.com/uploads/original/0/9253/2492773-drewbert.png",
-        "Matt Kessler": "https://static.giantbomb.com/uploads/original/0/9253/2492774-mattbodega.png",
-        "Andy McCurdy": "https://static.giantbomb.com/uploads/original/0/9253/2492772-andy.png"
+        "Dave Snider": "https://www.giantbomb.com/a/uploads/original/0/9253/2492775-snide.png",
+        "Drew Scanlon": "https://www.giantbomb.com/a/uploads/original/0/9253/2492773-drewbert.png",
+        "Matt Kessler": "https://www.giantbomb.com/a/uploads/original/0/9253/2492774-mattbodega.png",
+        "Andy McCurdy": "https://www.giantbomb.com/a/uploads/original/0/9253/2492772-andy.png"
     };
 
     if (reviewerImages.hasOwnProperty(reviewerName) && reviewerImages[reviewerName][numberScore]) {
